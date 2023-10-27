31f1ef422f6e338a74795ac18c0f2eb79c81b2b5
unknown <mendibass@gmail.com>
2023-10-27 21:47:17 +0300

    gen back to ./src/pages/Name

--- genConfig.js
+++ genConfig.js
@@ -25,7 +25,7 @@ generateTemplateFiles([
     stringReplacers: ['Name'],
     dynamicReplacers: [{slot:'__class__', slotValue: 'className'}],
     output: {
-      path: './src/pages/NamePage',
+      path: './src/pages/Name',
       pathAndFileNameDefaultCase: '(pascalCase)',
     },
   },
